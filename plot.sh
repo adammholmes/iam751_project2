1a023efeff55f8a7e0d6497e9526df1571ed88e6
Adam M. Holmes <adammholmes@outlook.com>
2014-05-15 19:41:27 -0400

    improving script portability

--- plot.sh
+++ plot.sh
@@ -21,14 +21,15 @@ LAST=$(( $TIMESTEPS - 1 ))
 echo "[2/4] Generating plots from data..."
 for i in `seq 0 $LAST`;
 do
-TIME=$(echo "$i * 0.0002" | bc -l)
+TIME=$(echo "$i 0.0002" | awk '{printf "%f", $1 * $2}')
+#TIME=$(echo "$i * 0.0002" | bc -l)
 gnuplot > /dev/null 2>&1 << EOF
 set term png
 set output "data/$i.png"
 set xrange [0:2]
 set yrange [0:2]
 set zrange [-1:1]
-set title "exp(-t) * sin(πx) * sin(πy)\ntime: 0$TIME"
+set title "exp(-t) * sin(pi*x) * sin(pi*y)\ntime: $TIME"
 set pm3d at s
 unset surface
 unset colorbox
@@ -42,7 +43,15 @@ done
 ### CREATE VIDEO
 echo "[3/4] Creating video from plots..."
 rm heatpde.mp4 > /dev/null 2>&1
-ffmpeg -start_number 0 -i data/%d.png heatpde.mp4 > /dev/null 2>&1
+# Some ffmpeg installs don't have '-start_number'
+if ffmpeg -start_number 0 -i data/%d.png heatpde.mp4 > /dev/null 2>&1; then
+    :
+else
+    ffmpeg -i data/%d.png heatpde.mp4 > /dev/null 2>&1
+fi
+
+
+
 
 
 ### CLEAN UP
